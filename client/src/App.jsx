1b67eb850bb0a133706308b8a62c21972e1c7617
Athul Shyam <athulshyam31@gmail.com>
2025-02-15 19:47:47 +0530

    ADDED SECONDARY CHANGES TO CLIENT

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
-// import { Routes, Route } from 'react-router-dom';
-// import HomePage from './pages/User/Home/';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/User/Home';
+import UserLogin from './pages/User/Login';
 // // import ListCategory from './pages/User/Category/List';
-// import UserLogin from './pages/User/Login';
 // import PrivateRoute from './components/PrivateRoute';
+
+import PublicNavbar from "./components/Navbar/Public";
 
 // import './App.css';
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
-    // <Routes>
-    //   <Route path="/user/login/" element={<UserLogin />} />
+    <>
+    <PublicNavbar/>
+    <Routes>
+      <Route path="/user/login/" element={<UserLogin />} />
 
-    //   <Route path="/user" element={<PrivateRoute />}>
-    //     <Route path="/user/home/" element={<HomePage />} />
-    //     <Route path="/user/category/" element={<ListCategory />} />
+      {/* <Route path="/user" element={<PrivateRoute />}> */}
+        <Route path="/user/home/" element={<HomePage />} />
+        {/* <Route path="/user/category/" element={<ListCategory />} /> */}
 
-    //   </Route>
-    // </Routes>
+      {/* </Route> */}
+    </Routes>
+    </>
   );
 };
 export default App;
